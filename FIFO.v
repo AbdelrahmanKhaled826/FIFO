138c39aaa3d540e41aff4ee729d0f2f2edafb7b1
Abdelrahman <66374409+AbdelrahmanKhaled826@users.noreply.github.com>
2023-08-30 03:53:10 +0300

    Add files via upload

---
+++ FIFO.v
@@ -0,0 +1,85 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:05:17 08/26/2023 
+// Design Name: 
+// Module Name:    FIFO 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FIFO
+#(
+parameter FIFO_DEPTH=8,
+ parameter DATA_WIDTH=32
+)
+( 
+input clk,reset,cs,
+input  wr_en,rd_en,
+input [DATA_WIDTH-1:0] data_in,
+output full,empty,
+output  reg [DATA_WIDTH-1:0] data_out
+);
+localparam DEPTH=clog2(FIFO_DEPTH);
+
+reg [DATA_WIDTH -1:0] fifo [0 : FIFO_DEPTH -1 ];
+ 
+ // Wr/Rd pointer have 1 extra bits at MSB
+ reg [DEPTH:0] write_pointer;
+ reg [DEPTH:0] read_pointer;
+ 
+
+
+always @(posedge clk or negedge reset) begin
+	if(! reset) 
+		begin
+		write_pointer<=0;
+		end
+	else if(cs && wr_en  && !full)
+		begin
+		write_pointer<=write_pointer+1'b1;
+		fifo[write_pointer[DEPTH-1:0]] <= data_in;
+		end
+end
+
+always @(posedge clk or negedge reset) begin
+	if(! reset) 
+		begin
+		read_pointer<=0;
+		data_out <= 0;
+		end
+	else if(cs && rd_en  && !empty)
+		begin
+		read_pointer<=read_pointer+1'b1;
+		data_out <= fifo[read_pointer[DEPTH-1:0]];
+		end
+end
+
+		
+assign empty =(read_pointer == write_pointer)? 1'b1:1'b0;
+
+assign full  = (read_pointer == {~write_pointer[DEPTH], write_pointer[DEPTH-1:0]});
+		
+
+//****************************************************
+// function for log 
+function integer clog2;
+  input integer value;
+  begin
+    value = value-1;
+    for (clog2=0; value>0; clog2=clog2+1)
+      value = value>>1;
+  end
+endfunction
+
+endmodule
---
+++ fifo_tb.v
@@ -0,0 +1,129 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   03:30:00 08/30/2023
+// Design Name:   FIFO
+// Module Name:   C:/example_verilog/d_ff/fifo_tb.v
+// Project Name:  d_ff
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FIFO
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module fifo_tb;
+
+parameter FIFO_DEPTH=8;
+ parameter DATA_WIDTH=32;
+	// Inputs
+	reg clk;
+	reg reset;
+	reg cs;
+	reg wr_en;
+	reg rd_en;
+	reg [31:0] data_in;
+
+	// Outputs
+	wire full;
+	wire empty;
+	wire [31:0] data_out;
+
+
+	integer i;
+	// Instantiate the Unit Under Test (UUT)
+	FIFO  #(.FIFO_DEPTH(8),
+	     .DATA_WIDTH(32))
+		FIFO(
+		.clk(clk), 
+		.reset(reset), 
+		.cs(cs), 
+		.wr_en(wr_en), 
+		.rd_en(rd_en), 
+		.data_in(data_in), 
+		.full(full), 
+		.empty(empty), 
+		.data_out(data_out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		reset = 0;
+		cs = 0;
+		wr_en = 0;
+		rd_en = 0;
+		data_in = 0;
+
+	end
+       task write_data(input [DATA_WIDTH-1:0] d_in);
+	    begin
+		    @(posedge clk); // sync to positive edge of clock
+			cs = 1; wr_en = 1;
+			data_in = d_in;
+			$display($time, " write_data data_in = %0d", data_in);
+			@(posedge clk);
+		    cs = 1; wr_en = 0;
+		end
+	endtask
+	
+	task read_data();
+	    begin
+		    @(posedge clk);  // sync to positive edge of clock
+			cs = 1; rd_en = 1;
+			@(posedge clk);
+			#0.1;
+		    $display($time, " read_data data_out = %0d", data_out);
+		    cs = 1; rd_en = 0;
+		end
+	endtask
+	
+	// Create the clock signal
+	always begin #0.5 clk = ~clk; end
+	
+    // Create stimulus	  
+    initial begin
+	    #1; 
+		reset = 0; rd_en = 0; wr_en = 0;
+		
+		#1.3; 
+		reset = 1;
+		$display($time, "\n SCENARIO 1");
+		write_data(1);
+		write_data(10);
+		write_data(100);
+		read_data();
+		read_data();
+		read_data();
+		read_data();
+		
+        $display($time, "\n SCENARIO 2");
+		for (i=0; i<FIFO_DEPTH; i=i+1) begin
+		    write_data(2**i);
+			read_data();        
+		end
+
+        $display($time, "\n SCENARIO 3");		
+		for (i=0; i<=FIFO_DEPTH; i=i+1) begin
+		    write_data(2**i);
+		end
+		
+		for (i=0; i<FIFO_DEPTH; i=i+1) begin
+			read_data();
+		end
+		
+	    #40 $stop;
+	end
+      
+endmodule
+
